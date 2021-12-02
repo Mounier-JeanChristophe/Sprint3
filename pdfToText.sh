c4e39cdabea37b0820e846f75933a1d65d10361d
Marie <marie@mariezephir@example.com>
2021-12-02 12:08:32 +0100

    version finale du scriptbash sans erreur lors du déplacement des fichiers .txt

--- pdfToText.sh
+++ pdfToText.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-if [ ! -d "Text" ] 
-then
-    $(mkdir Text)
-fi
+	if [ ! -d "TEXT" ] 
+		then
+			$(mkdir TEXT/)
+	fi
 
-for fich in $(find -name "*.pdf")
-do
-    $(pdftotext $fich)
-done
+	for fich in $(find -name "*.pdf")
+	do
+		$(pdftotext $fich)
+	done
 
-for fich in $(find -name "*.txt")
-do
-    $(mv $fich Text/)
-done
+	for fich in $(find CORPUS_TRAIN -name "*.txt" | grep -oP '(?<=/)[^ ]*')
+	do
+		if [ ! -f TEXT/$fich ]
+			then 
+				mv CORPUS_TRAIN/$fich TEXT
+		else
+			rm TEXT/$fich
+			mv CORPUS_TRAIN/$fich TEXT
+		fi
+	done
+	
